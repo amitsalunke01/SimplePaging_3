a0f8d40a56e59d0066b062addf23b63f50574991
AMIT\AMIT <amitsalunke01@gmail.com>
2021-09-06 17:50:30 +0530

    Project Upload

---
+++ app/src/main/java/com/amitsalunke/network/RetroInstance.kt
@@ -0,0 +1,16 @@
+package com.amitsalunke.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetroInstance {
+    companion object {
+        val baseUrl = "https://rickandmortyapi.com/api/"
+        fun getRetroInstance():Retrofit{
+            return Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+    }
+}
